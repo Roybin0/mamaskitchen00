d7f6b8d34448a6e76e9a0d5ccf8ec3f955258ec3
Robyn Smith <robyn.roybin@gmail.com>
2023-03-18 13:19:31 +0000

    Customise account templates

--- bookings/forms.py
+++ bookings/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Booking
+from allauth.account.forms import SignupForm
 
 
 class BookingForm(forms.ModelForm):
@@ -14,3 +15,19 @@ class BookingForm(forms.ModelForm):
                            'placeholder': 'Select a date',
                            'type': 'date'}
                     )}
+
+
+class SignupForm(SignupForm):
+    name = forms.CharField(max_length=50, label='Name:')
+    phone = forms.IntegerField(label='Phone No.:')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['email'] = forms.EmailField(label='Email:')
+
+    def save(self, request):
+        user = super(SignupForm, self).save(request)
+        user.name = self.cleaned_data['name']
+        user.phone = self.cleaned_data['phone']
+        user.save()
+        return user
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,16 +1,6 @@
 var medium_screen = window.matchMedia("(max-width: 768px)")
 var small_screen = window.matchMedia("(max-width: 576px)")
 
-function editLabelForSignup() {
-    let id = 'id_email';
-    labels = document.getElementsByTagName('label');
-    for (i = 0; i < labels.length; i++) {
-        if (labels[i].htmlFor == id) {
-            document.getElementById('id_email').label.innerHTML = 'Email:';
-        }
-    }
-}
-
 function changeFooter(screen_size) {
     if (screen_size.matches) { 
         footer = document.getElementById("footer"); 
