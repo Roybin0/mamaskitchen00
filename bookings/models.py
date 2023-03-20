80c8a2744f8717cf0e59d19a7aefc48f03b861dc
Robyn Smith <robyn.roybin@gmail.com>
2023-03-20 02:00:07 +0000

    Updated date field

--- bookings/models.py
+++ bookings/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import User
-from cloudinary.models import CloudinaryField
 from django.core.validators import MinValueValidator, MaxValueValidator
 from datetime import datetime, date
 import random
